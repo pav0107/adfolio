a4213919ff731d17ab25c4d92fdba32cd4538fcb
Pav <pav0107@gmail.com>
2023-04-08 23:08:07 +0100

    wip

--- src/app/Client.js
+++ src/app/Client.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 function Client({ client, img, alt }) {
   return (
-    <div class="relative flex justify-center items-center w-1/3 h-80 group cursor-pointer">
+    <div className="relative flex items-center justify-center w-1/3 cursor-pointer h-80 group">
       <Image
         src={img}
         alt={alt}
@@ -15,11 +15,11 @@ function Client({ client, img, alt }) {
       />
 
       <Link href={`/${client.replace(/[/\s]/g, '')}`}>
-        <h1 class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white text-3xl font-bold z-[3] hidden group-hover:block">
+        <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white text-3xl font-bold z-[3] hidden group-hover:block">
           {client}
         </h1>
         <div
-          class="absolute top-0 left-0 right-0 bottom-0
+          className="absolute top-0 left-0 right-0 bottom-0
          bg-black transition-opacity opacity-0 group-hover:opacity-70 duration-500 ease-in-out z-[2]"
         ></div>
       </Link>
