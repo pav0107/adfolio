231f4e69e8d80a90fbf955c334d3f2e4e3b4ebf3
Pav <pav0107@gmail.com>
2023-04-08 23:00:50 +0100

    wip

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -2,7 +2,45 @@ import React from 'react';
 import Nav from '../Nav';
 
 function page() {
-  return <div>Hi, my name's Pav.</div>;
+  return (
+    <div className="flex flex-col items-center justify-center m-8">
+      <div className="max-w-xl">
+        <ul className="list-disc">
+          <li className="py-1">
+            Graduate of Tony Cullingham's Watford ad school.
+          </li>
+          <li className="py-1">
+            Placements at Mother, Wieden + Kennedy and DDB amongst others.
+          </li>
+          <li className="py-1">
+            Featured in Creative Review’s ‘Creative Futures’ showcase.
+          </li>
+          <li className="py-1">
+            Given my first job by Paul Belford at This is Real Art.
+          </li>
+          <li className="py-1">
+            Previous writing includes headlines, naming initiatives, signage,
+            essays, film narration, radio, mission statements, brand guidelines
+            and letters.
+          </li>
+          <li className="py-1">
+            Experience across a variety of media such as TV, posters, press,
+            digital, documentary film, T-shirts, leaflets, events, letters,
+            radio and video.
+          </li>
+          <li className="py-1">
+            Performed stand-up comedy across the UK and in Europe to audiences
+            of up to 400 people, winning the Bath Comedy Festival’s New Act of
+            the Year competition and getting to the semi-final of the BBC’s New
+            Comedy Awards.
+          </li>
+        </ul>
+
+        <p className="mt-8 mb-2 font-bold">pav0107@gmail.com</p>
+        <p className="font-bold">07799 113911</p>
+      </div>
+    </div>
+  );
 }
 
 export default page;
