6cb16ff497dcf25d47ae667bae5084cb0a06420b
Pav <pav0107@gmail.com>
2023-07-30 10:22:54 +0100

    change background for mobile

--- src/app/Client.js
+++ src/app/Client.js
@@ -15,12 +15,12 @@ function Client({ client, img, alt }) {
       />
 
       <Link href={`/${client.replace(/[/\s]/g, '')}`}>
-        <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white text-3xl font-bold z-[3] sm:hidden group-hover:block">
+        <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white text-3xl font-bold z-[3] sm:hidden sm:group-hover:block">
           {client}
         </h1>
         <div
           className="absolute top-0 left-0 right-0 bottom-0
-         bg-black transition-opacity opacity-0 group-hover:opacity-70 duration-500 ease-in-out z-[2]"
+         bg-black opacity-70 sm:bg-black sm:transition-opacity sm:opacity-0 sm:group-hover:opacity-70 sm:duration-500 sm:ease-in-out z-[2]"
         ></div>
       </Link>
       {/* 
